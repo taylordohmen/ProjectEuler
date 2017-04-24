621d8bf0a8aa58990c6876bff69b138c6cfd0f8d
Taylor Dohmen <tdohmen@mail.umw.edu>
2017-04-23 23:42:12 -0400

    what is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%

---
+++ Euler58.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+def is_prime(n):
+	for i in range(2, int(sqrt(n)) + 1):
+		if n % i == 0:
+			return False
+	return True
+
+n = 7
+n0 = n
+primes = 8
+while primes/((2*n)-1) >= .10:
+	n += 2
+	a = n**2 - n + 1
+	b = a - n + 1
+	c = b - n + 1
+	corners = [a,b,c]
+	for i in corners:
+		if is_prime(i):
+			primes += 1
+
+print(n)
+print(primes/((2*n)-1))
