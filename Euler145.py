833eb45b4f5868dce14af126374d261900691664
Taylor Dohmen <tdohmen@mail.umw.edu>
2017-04-26 11:29:44 -0400

    How many reversible numbers are there below one-billion?(3)

--- Euler145.py
+++ Euler145.py
@@ -1,13 +1,11 @@
 import re
-odd_form = re.compile('[13579]*')
+even_form = re.compile('[13579]*')
 def count_range(x, y):
 	count = 0
-	for n in (i for i in range(x, y, 2) if i % 10 != 0):
+	for n in range(x, y):
+		if n % 10 == 0: continue
 		some = str(n + int(str(n)[::-1]))
-		if odd_form.fullmatch(some): count += 2
+		if even_form.fullmatch(some): count += 1
 	return count
 
-print(count_range(1, 1000))
-
-# just ran this func once per valid |n| so 3,4,6,7,8 and added them up
-# not an awesome solution but a solution none the less
+print(count_range(10000000, 100000000))
