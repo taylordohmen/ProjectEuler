f272ff7e49d7643cf8bcd88e44c390c005095001
taylordohmen <tdohmen@mail.umw.edu>
2017-08-02 10:11:46 -0400

    big performance improvement

--- Euler69.py
+++ Euler69.py
@@ -4,21 +4,24 @@ lim = 1000001
 
 def primes(n):
 	P = [2]
+	pset = {2}
 	p = 3
 	while len(P) < n:
 		i = 0
 		prime = True
-		while P[i] <= sqrt(p):
+		s = sqrt(p)
+		while P[i] <= s:
 			if p % P[i] == 0:
 				prime = False
 				break
 			i += 1
 		if prime:
 			P.append(p)
+			pset.add(p)
 		p += 2
-	return P
+	return P, pset
 
-P = primes(20 * int(sqrt(lim)) + 1)
+P, pset = primes(20 * int(sqrt(lim)) + 1)
 
 def prime_factors(n):
 	global P
@@ -32,12 +35,17 @@ def prime_factors(n):
 		while n % m == 0:
 			f[m] += 1
 			n //= m
+		if n in pset:
+			f[n] = 1
+			break
 		i += 1
 		m = P[i]
 	return f
 
 def phi(n):
 	coprime = 1
+	if n in pset:
+		return coprime
 	primes = prime_factors(n)
 	for p in primes:
 		if primes[p] > 1:
